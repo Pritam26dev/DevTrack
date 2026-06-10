ed395f90134e9a8ec0bd2105aa83996728107299
Pritam Debnath <24155487@kiit.ac.in>
2026-06-10 23:39:22 +0530

    integrated architecture

---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    
+    const { username, email, password } = req.body;
+
+    
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
