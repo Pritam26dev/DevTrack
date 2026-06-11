d8fb2e902d80765c880826ee8ab4dd7d83e2b29c
Pritam Debnath <24155487@kiit.ac.in>
2026-06-11 21:51:52 +0530

    signup and login middleware

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt=require("jsonwebtoken")
 
 const registerUser = async (req, res) => {
   try {
@@ -50,6 +51,54 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser=async(req,res)=>{
+  try{
+    const {email,password}=req.body;
+
+    if(!email || !password){
+      return res.status(400).json({
+        message:"all fields are required"
+      })
+    }
+
+    const user=await User.findOne({email});
+
+    if(!user){
+      return res.status(400).json({
+        message:"invalid credentials"
+      })
+    }
+    const isMatch=await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      return res.status(400).json({
+        message:"invalid credentials"
+      })
+    }
+    const token=jwt.sign({
+      id:user._id,
+    },
+  process.env.JWT_SECRET,{
+    expiresIn:"7d"})
+
+    res.status(200).json({
+      message:"Login succesful",
+      token,
+      user:{
+        id:user._id,
+        username:user.username,
+        email:user.email,
+      }
+    })
+
+  }catch(error){
+    console.error(error);
+
+    res.status(500).json({
+      message:"server error"
+    })
+  }
+}
+
 module.exports = {
-  registerUser,
+  registerUser,loginUser
 };
